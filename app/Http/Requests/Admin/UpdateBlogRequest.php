02bf3f3394bb066ee592d63e99208bf638c1f7ae
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-11-14 21:19:08 +0900

    blogsテーブルにuser_idをカラム追加してブログ一覧に投稿したユーザー名を表示するように機能追加(PR指摘7: 共通のAttributesを共通トイレトに移行)

--- app/Http/Requests/Admin/UpdateBlogRequest.php
+++ app/Http/Requests/Admin/UpdateBlogRequest.php
@@ -39,15 +39,4 @@ class UpdateBlogRequest extends FormRequest
             'cats.*' => ['distinct', 'exists:cats,id'],
         ];
     }
-
-    public function attributes()
-    {
-        return [
-            'title' => 'タイトル',
-            'image' => '画像',
-            'category_id' => 'カテゴリー',
-            'body' => '本文',
-            'cats' => '登場するねこ',
-        ];
-    }
 }
