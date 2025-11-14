3878853a5d8abe3843c4474bb97faedf4ac13e82
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-11-14 19:19:15 +0900

    blogsテーブルにuser_idをカラム追加してブログ一覧に投稿したユーザー名を表示するように機能追加(PR指摘6)

--- app/Http/Requests/Admin/StoreBlogRequest.php
+++ app/Http/Requests/Admin/StoreBlogRequest.php
@@ -44,6 +44,7 @@ class StoreBlogRequest extends FormRequest
             'title' => 'タイトル',
             'image' => '画像',
             'category_id' => 'カテゴリー',
+            'body' => '本文',
         ];
     }
 }
